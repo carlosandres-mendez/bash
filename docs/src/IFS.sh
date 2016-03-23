ded57c342939fc5d94177ec42b314e19275b6bad
Carlos Andrés Méndez <carlosandresucr@gmail.com>
2016-03-22 20:31:05 -0600

    Listo bloque de conversion de datos. Además, de los datos en csv se tienen organizaciones dentro de la carpeta hojasDatos

---
+++ docs/src/IFS.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+VAR1="algo:otro guia, paso, calle"
+
+# Por defecto el Internal Field Separator es
+#IFS=$' \n\t'
+# Realizar la prueba con dos delimitadores ":" y ","
+IFS=:,
+
+for dato in $VAR1; do
+	echo "Dato: $dato"
+	
+	# Para correr este ejemplo se deben definir las variables
+	# VAR2 y VAR3 antes de correr el script. Hagalo de dos
+	# formas distitas sin definirlas dentro del script
+	echo "La variable dos contiene \"$VAR2\" y \
+tres contiene \"$VAR3\"."
+done
+
