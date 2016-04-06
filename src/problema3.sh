78e960313a4eb0f35241bd1b493524972a6e7c45
carlosandres-mendez <carlosandresucr@gmail.com>
2016-04-05 20:17:32 -0600

    Subir problema 2 terminado

--- src/problema3.sh
+++ src/problema3.sh
@@ -4,16 +4,16 @@
 
 DATOS="../problema3/Datos"
 
-SALIDA_DATOS="../problema3/datos_csv"
+SALIDA_DATOS="../problema3/datos_csv/datos.out"
 
-mkdir $SALIDA_DATOS
+mkdir ../problema3/datos_csv
 
 echo "Procesando archivo Datos"
 #: <<'END'
 
-cat $DATOS | tail -n 100 | awk -F "," '{print $1 " " $4 " " $5}' | tr -d '"' >> $SALIDA_DATOS/datos.out
+cat $DATOS | awk -F "," '{print $1 " " $4 " " $5 " " $9 " " $11 " " $12 " " $13 " " $14 " " $15 " " $16}' | tr -d '"' >> $SALIDA_DATOS
 #echo "$LUZ $AGUA" >> $SALIDA_DATOS/datos.out
-
+#| tail -n 2400
 
 
 #cat $SALIDA_DATOS/datos.out 
@@ -22,7 +22,7 @@ cat $DATOS | tail -n 100 | awk -F "," '{print $1 " " $4 " " $5}' | tr -d '"' >> 
 
 #FMT_BEGIN="20110205 0000"
 #FMT_END="20110209 0200"
-#FMT_X_SHOW="%d/%H"
+FMT_X_SHOW="%d" # Mostrar solo el dia en la fecha del eje x
 
 graficar()
 {
@@ -31,14 +31,14 @@ graficar()
 	set timefmt "%Y-%m-%d"
 	#set xrange ["$FMT_BEGIN" : "$FMT_END"]
 	set autoscale
-	#set format x "$FMT_X_SHOW"
+	set format x "$FMT_X_SHOW"
 	set terminal png
 
 	set xlabel "Fecha"
 	set ylabel "Indice de radiacion solar"
-	set output '/var/www/ciem/fig-indice.png'
+	set output 'fig-indice.png'
 	#set xrange ["1" : "3"] # Muestra el consumo electrico en los 3 primeros meses
-	plot "$SALIDA_DATOS/datos.out" using 1:3 with lines title "Variable no se que es", "$SALIDA_DATOS/datos.out" using 1:4 with lines title "Variable menos que se"
+	plot "$SALIDA_DATOS" using 1:3 with lines title "Var 1", "$SALIDA_DATOS" using 1:4 with lines title "Var 2", "$SALIDA_DATOS" using 1:5 with lines title "Var 3", "$SALIDA_DATOS" using 1:7 with lines title "Var 4", "$SALIDA_DATOS" using 1:8 with lines title "Var 5", "$SALIDA_DATOS" using 1:9 with lines title "Var 6", "$SALIDA_DATOS" using 1:10 with lines title "Var 7", "$SALIDA_DATOS" using 1:11 with lines title "Var 8", "$SALIDA_DATOS" using 1:12 with lines title "Var 9"
 EOF
 }
 
